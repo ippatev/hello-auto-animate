684731a79a9691c73b5399f5ae2eb8dc473ae894
ippatev <alexanderippatiev24@gmail.com>
2022-06-03 10:19:00 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from 'vue'
+
+const items = ref(["😏","😐","😑","😒"])
+
+function removeItem(toRemove) {
+  items.value = items.value.filter((i) => i !== toRemove)
+}
+</script>
+
+<template>
+  <ul v-auto-animate>
+    <li
+        v-for="item in items"
+        :key="item"
+        @click="removeItem(item)"
+    >
+      {{ item }}
+    </li>
+  </ul>
+</template>
+
+<style>
+ul {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  list-style: none;
+}
+
+ul > li {
+  padding: 2em;
+  border: 1px solid black;
+  cursor: pointer;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
+import App from './App.vue'
+
+createApp(App).use(autoAnimatePlugin).mount('#app')
